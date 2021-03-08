48beef1f2d81c3a453cae8a812ef0450c4a2bb17
Againyunn <76543591+Againyunn@users.noreply.github.com>
2021-03-09 00:30:51 +0900

    1차
    programmers level1
    Complete

--- 그누빌/알고리즘/1차/(생각2)두개 뽑아서 더하기.py
+++ 그누빌/알고리즘/1차/(생각2)두개 뽑아서 더하기.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 def solution(numbers):
     n=numbers
     r1=[]#r1은 두 원소의 합을 담은 리스트   --> 굳이 none값으로 빈리스트를 경우의 수(조합)만큼 생성하여 차후에 원소에 값을 추가할 필요 없이 바로 빈리스트 생성 후 나중에 append로 추가하는 방법 이용가능
@@ -12,5 +14,10 @@ def solution(numbers):
     for i in r1:
         if i not in r2:
             r2.append(i)
-    answer= sorted(r2)
-    return answer
+    answer=sorted(r2)
+    print(answer)
+                
+if __name__=='__main__':
+    numbers=int(input())
+
+    solution(numbers)
--- 그누빌/알고리즘/1차/(완성)모의고사.py
+++ 그누빌/알고리즘/1차/(완성)모의고사.py
@@ -1,56 +1,36 @@
-from typing import Sequence
-
-def solution(q, num1, num2, num3 : Sequence):
-    num1 = list(map(int,num1)) #1번 수포자의 정답 문제
-    num2 = list(map(int,num2)) #2번 수포자의 정답 문제
-    num3 = list(map(int,num3)) #3번 수포자의 정답 문제
+def solution(answers):
+    q=answers
+    num1 = [1,2,3,4,5] #1번 수포자의 정답 문제
+    num2 = [2,1,2,3,2,4,2,5] #2번 수포자의 정답 문제
+    num3 = [3,3,1,1,2,2,4,4,5,5] #3번 수포자의 정답 문제
     r1=0 #1번 수포자의 정답 횟수 기록
     r2=0 #2번 수포자의 정답 횟수 기록
     r3=0 #3번 수포자의 정답 횟수 기록
     for i in range(len(q)):
-        if q[i] == num1[i]:
+        if q[i] == num1[i%5]:
             r1+=1
-        if q[i] == num2[i]:
+        if q[i] == num2[i%8]:
             r2+=1
-        if q[i] == num3[i]:
+        if q[i] == num3[i%10]:
             r3+=1
     winner = []
     if r1> r2:
         if r1>r3:
-            winner.append(1)
+            winner=[1]
         elif r1<r3:
-            winner.append(3)
+            winner=[3]
         else: #r1==r3
-            winner.append(1)
-            winner.append(3)
+            winner=[1,3]
     elif r2> r1:
         if r2>r3:
-            winner.append(2)
+            winner=[2]
         elif r2<r3:
-            winner.append(3)
+            winner=[3]
         else:
-            winner.append(2)
-            winner.append(3)
+            winner=[2,3]
     else:#r1=r2인 경우
         if r1==r3:
-            winner.append(1)
-            winner.append(2)
-            winner.append(3)
+            winner=[1,2,3]
         else:
-            winner.append(1)
-            winner.append(2)
-    print(winner)
-
-if __name__=='__main__':
-    while True:
-        q = list(input('시험의 답을 입력하시오. : '))
-        q=list(map(int,q))
-        for i in range(len(q)):
-            if 1<=q[i]<=5:
-                break
-        break
-    num1 = list(input('1번 수포자의 답을 입력하시오. : '))    
-    num2 = list(input('2번 수포자의 답을 입력하시오. : '))  
-    num3 = list(input('3번 수포자의 답을 입력하시오. : ')) 
-    solution(q, num1, num2, num3)
-
+            winner=[1,2]
+    return winner
