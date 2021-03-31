e8d7c8335ffb7d04166fb3b355812c6f0835d74e
Againyunn <76543591+Againyunn@users.noreply.github.com>
2021-03-31 13:21:20 +0900

    2021.03.31
    그누빌 알고리즘 leve2 순위검색
    정리

--- 그누빌/알고리즘/4차/순위검색.py
+++ 그누빌/알고리즘/4차/순위검색.py
@@ -1,72 +1,73 @@
-# info 가 조건
-# query 가 요구사항
-# - 는 미선택을 의미한다.
-
-#################################################
-# (아래의 과정을 문제 5개의 질문 변수로 미리 만들기)
-# 데이터를 각 리스트에 옮겨 담기
-# 각 데이터별 조회할 데이터를 변수에 담기
-# 조회할 변수의 질문을 변수로 만들기
-
-# 질문을 if로 묶어서 실행
-# 만약 질문이 없어서 - 값인 경우 해당 질문을 건너뛰도록 알고리즘 구현
-
-
 def solution(info, query):
-    #사전 작업
-    data=[None]*len(info) #info의 데이터를 나누어 담을 빈 리스트
-    for i in range(len(info)): #info의 " "로 묶인 원소를 data의 원소로 담기
-        data[i]=info[i].split #2중 배열로 저장
-
-    inquire=[None]*len(query) #query의 요구사항을 나누어 담을 빈 리스트
+    #사전 작업 : info의 " "로 묶인 원소를 data의 원소로 / query의 " "로 묶인 원소를 inquire의 원소로
+    data=[] #info의 데이터
+    for i in range(len(info)): 
+        transform11=str(info[i]).strip('[ ,"]') #info의 각 원소를 str으로 변환 후 불필요한 기호 제거
+        if transform11 not in ('['):
+            data.append(transform11.split()) #유의미한 값들만 data에 저장
+    inquiry=[] #query의 요구사항
     for i in range(len(query)):
-        inquire[i]=query[i].split
-        inquire[i].remove('and')
+        transform21=str(query[i]).strip('[ ,"]')
+        transform22=transform21.replace("and","") #query의 각 원소를 str으로 변환 후 불필요한 기호 제거
+        if transform22 not in ('[') :
+            inquiry.append(transform22.split()) #유의미한 값들만 inquire에 저장
 
-    #조회할 질문의 명단 만들기
-    count=[] #경우에 따른 명 수를 체크하기 위한 빈 리스트
-    #for 구문으로 반복하기 (로직 완성 후 변환하기)
-    for a in range(len(query)):
-        #우선은 첫문제의 로직을 세우기 위해 [0]의 경우에 데이터 변환부터 처리
-        
-        #1)프로그래밍 언어 구분: 
-        allLanguage=['cpp','java','python','-'] #전체 프로그래밍 언어
-        language=allLanguage.index(inquire[a]) #질문에서 묻는 언어를 langauge에 저장
-        #질문1의 유형 정리
-        #question1 = language in data[a][0] #a번째 질문의 언어가 languge와 맞는 지 확인
+    #개수
+    dataNum=len(data) #지원자 수
+    inquiryNum=len(inquiry) #요구사항의 수
 
+    #각 쿼리(요구사항)별 체크사항
+    check=[0]*inquiryNum #각 원소가 각각의 쿼리 별 체크사항을 의미
+    score=[]#점수를 기록용 빈리스트(점수만 integer형이므로 따로 생각)
+
+    #inquire리스트의 원소가 지칭하는 바 
+    # inquire 1중 리스트 : 각 원소 = data의 지원자 개체 , 2중 리스트 : 각 원소 = inquire의 요구사항마다의 충족여부
+    # inquire[a][0: 언어 , 1: 직군, 2: 경력, 3: 소울푸드, 4:점수]
+    # inquire의 요구사항이 '미선택'이 아닌 경우에만 check +1
+    for a in range(inquiryNum):
+        #1)프로그래밍 언어 구분:  
+        if inquiry[a][0] != '-': 
+            check[a]+=1
         #2)직군 구분:
-        allPosition=['backend','frontend','-']  #전체 직군
-        position=allPosition.index(inquire[a])
-        #질문2의 유형 정리
-        #question2 = position in data[a][1] #a번째 질문의 직군이 position과 맞는 지 확인
-        
+        if inquiry[a][1] != '-':
+            check[a]+=1
         #3)경력 구분:
-        allCareer=['junior','senior','-']
-        career=allCareer.index(inquire[a])
-        #질문3의 유형 정리
-        #question3 = career in data[a][2]
-
+        if inquiry[a][2] != '-':
+            check[a]+=1
         #4)소울푸드 구분:
-        allFood=['chicken','pizza','-']
-        food=allFood.index(inquire[a])
-        #질문4의 유형 정리
-        #question4 = food in data[a][3]
-
-        #5)점수 조회
-        score = data[a][4]
-
-
-
-
-
-
-
-
-
-
-
-
-
-    answer = []
+        if inquiry[a][3] != '-':
+            check[a]+=1
+        #5)점수 조회:
+        score.append(int(inquiry[a][4])) #inquire은 str타입이므로 int형 변환 후 score저장
+        check[a]+=1
+    
+    # 퀴리별 체크사항에 부합하는 info 판별하여 count에 기록
+    count=[0]*dataNum #경우에 따른 명 수를 체크하기 위한 빈 리스트(각 info 원소가 쿼리별로 얼마나 충족하는 지 판별)
+    
+    for i in range(dataNum):
+        countMini=[0]*inquiryNum # 지원자마다 쿼리별 요구사항 충족정도를 기록하기 위한 리스트 
+        for j in range(inquiryNum): #각 지원자별 체크사항과 부합하는 지 여부 확인
+            if inquiry[j][0]!='-' and inquiry[j][0]==data[i][0]:
+                    countMini[j]+=1
+            if inquiry[j][1]!='-' and inquiry[j][1]==data[i][1]:
+                    countMini[j]+=1
+            if inquiry[j][2]!='-' and inquiry[j][2]==data[i][2]:
+                    countMini[j]+=1
+            if inquiry[j][3]!='-' and inquiry[j][3]==data[i][3]:
+                    countMini[j]+=1
+            if score[j]<=int(data[i][4]): #score과 같거나 더 큰 점수를 가진 경우
+                countMini[j]+=1
+            count[i]=countMini
+    
+    #각 쿼리별 체크사항을 취합한 count와 check(쿼리 요구사항)이 일치하는 지 판별
+    answer = [0]*inquiryNum #질문 개수대로 0 생성(각 index가 쿼리의 문항을 의미)
+    for i in range(inquiryNum): 
+        for j in range(dataNum): 
+            if count[j][i]==check[i]:
+                answer[i]+=1
     return answer
+
+info=input("info : ").split('"')
+query=input("query : ").split('"')
+sol=solution(info,query)
+print(sol)
