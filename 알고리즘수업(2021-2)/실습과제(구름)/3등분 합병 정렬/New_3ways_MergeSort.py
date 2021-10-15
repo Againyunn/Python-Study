93887ef48a5b7d9778246d33fc2c9cfd670d51f9
Jaeyun Jung <rangyun36@gmail.com>
2021-10-15 12:22:06 +0900

    21.10.15 Algorithm

--- 알고리즘수업(2021-2)/실습과제(구름)/3등분 합병 정렬/New_3ways_MergeSort.py
+++ 알고리즘수업(2021-2)/실습과제(구름)/3등분 합병 정렬/New_3ways_MergeSort.py
@@ -1,4 +1,7 @@
 def merge(A, i, j, k, l):
+    #입력값의 가정: i<=j , j<k<=l
+    #따라서 mid값은 j로 가정하고, 연산에 쓰일 k값은 k(j+1)그대로 적용
+    #각 변수가 오버라이딩될 위험 방지를 위해 가감이 발생하는 변수는 따로 저장
     mid=j
     set_i=i
     set_k=k
@@ -24,19 +27,22 @@ def m_sort(A, first, last):
     if last-first <1:
         return
 
-
+    #1/3값 지정
     oneThird=first+(last-first)//3
+
+    #2/3값 지정
     twoThird=first+2*((last-first)//3)+1
 
+    #1/3씩 기준으로 3등분하여 각각 재귀 정렬
     m_sort(A, first, oneThird)
     m_sort(A, oneThird+1, twoThird)
     m_sort(A, twoThird+1, last)
 
+    #1차 합병(처음~1/3 & 1/3+1~ 2/3)
     merge(A, first, oneThird, oneThird + 1, twoThird)
-    print(f'merge={A}')
-
+    #2차 합병(1차 합병 결과 & 2/3+1~끝)
     merge(A, first, twoThird, twoThird + 1, last)
-    print(f'merge={A}')
+
 
 
 
