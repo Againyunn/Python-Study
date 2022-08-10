05c2e3b30fd32517204b120646ecdcb1a8da2ce1
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-08-10 12:36:15 +0900

    Create: 코딩테스트 중급반 4회차 BFS 문제 풀이1

---
+++ 코딩테스트 대비 중급반(22-하계방학)/중급반 실습/BFS/네 방향 탈출 가능 여부 판별하기.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+
+VERTICES_NUM = n
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0] * m
+    for _ in range(n)
+]
+
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    
+    if visited[x][y] or grid[x][y] == 0:
+        return False
+    
+    return True
+
+def push(x, y):
+
+    visited[x][y] = 1
+    q.append((x, y))
+
+def bfs():
+    dxs = [1, 0, -1, 0]
+    dys = [0, 1, 0, -1]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y + dy
+
+            if can_go(new_x, new_y):
+                push(new_x, new_y)
+
+
+push(0, 0)
+bfs()
+print(visited[n - 1][m - 1])
